19a9bb8fe20cdbf651276fae9c5bc68be575b2e2
Gleyson Nunes <151650325+gleysonnunez@users.noreply.github.com>
2025-06-07 20:40:27 +0000

    feat: create access log insertion operation

--- src/main/java/com/bradesco/antifraud/service/AccessLogService.java
+++ src/main/java/com/bradesco/antifraud/service/AccessLogService.java
@@ -1,11 +1,17 @@
 package com.bradesco.antifraud.service;
 
 import com.bradesco.antifraud.model.AccessLog;
+import com.bradesco.antifraud.model.Customer;
 import com.bradesco.antifraud.repository.AccessLogRepository;
+
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
@@ -13,6 +19,7 @@ import java.util.UUID;
 public class AccessLogService {
 
     private final AccessLogRepository repository;
+    private final CustomerService customerService;
 
     public AccessLog create(AccessLog log) {
         return repository.save(log);
@@ -22,4 +29,23 @@ public class AccessLogService {
         return repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Access log not found"));
     }
+
+    public AccessLog createLog(UUID customerId, HttpServletRequest request) {
+    Customer customer = customerService.findById(customerId);
+
+    String ip = getClientIp(request);
+    String userAgent = request.getHeader("User-Agent");
+    String path = request.getRequestURI();
+    LocalDateTime accessTime = LocalDateTime.now();
+
+    AccessLog log = AccessLog.builder()
+            .customer(customer)
+            .ipAddress(ip)
+            .userAgent(userAgent)
+            .path(path)
+            .accessTime(accessTime)
+            .build();
+
+    return repository.save(log);
+}
 }
