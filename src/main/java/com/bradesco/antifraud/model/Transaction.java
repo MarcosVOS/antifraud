dfe69043b63f9e5ba2e765908858c9861d2e402c
Antônio Miguel <antoniomigueldossantos0@gmail.com>
2025-06-09 03:27:39 +0000

    fix:transaction

--- src/main/java/com/bradesco/antifraud/model/Transaction.java
+++ src/main/java/com/bradesco/antifraud/model/Transaction.java
@@ -12,18 +12,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 
-import lombok.Builder; 
-import lombok.Data;    
-
 @Entity
-@Data 
-@Builder 
 public class Transaction {
-
+    
     @Id
     @GeneratedValue
     private UUID id;
-
+    
     public enum TransactionType{
         DEPOSITO,
         SAQUE,
@@ -39,10 +34,59 @@ public class Transaction {
     @NotNull
     private LocalDateTime dataHora;
     private String descricao;
-
+    
     @ManyToOne
     private Account contaDeOrigem;
     @ManyToOne
     private Account contaDeDestino;
+    
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public TransactionType getTipo(){
+        return tipo;
+    }
+    public void setTipo(TransactionType tipo){
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public Account getContaDeOrigem() {
+        return contaDeOrigem;
+    }
+    public void setContaDeOrigem(Account contaDeOrigem) {
+        this.contaDeOrigem = contaDeOrigem;
+    }
+    public Account getContaDeDestino() {
+        return contaDeDestino;
+    }
+    public void setContaDeDestino(Account contaDeDestino) {
+        this.contaDeDestino = contaDeDestino;
+    }
 
 }
