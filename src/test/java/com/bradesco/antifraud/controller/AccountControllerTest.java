3b9a5dcd94c448edd60d8140f8e5210b918f059c
MarcosVOS <marcos.oliveiradev@gmail.com>
2025-06-08 23:44:07 -0300

    test(info): disable Spring Security filters for InfoController tests

--- src/test/java/com/bradesco/antifraud/controller/AccountControllerTest.java
+++ src/test/java/com/bradesco/antifraud/controller/AccountControllerTest.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AccountController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AccountControllerTest {
 
     @Autowired
